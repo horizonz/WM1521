855fd00acb5125737ac262e1467d88e824d63268
qianfeng0 <qianfeng0@W-M.local>
2016-04-01 15:10:19 +0800

    A创建了personA类

---
+++ text1/text1/personA.h
@@ -0,0 +1,13 @@
+//
+//  personA.h
+//  text1
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 MengWei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface personA : NSObject
+
+@end
---
+++ text1/text1/personA.m
@@ -0,0 +1,13 @@
+//
+//  personA.m
+//  text1
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 MengWei. All rights reserved.
+//
+
+#import "personA.h"
+
+@implementation personA
+
+@end
