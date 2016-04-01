796b332a662239335ca08c4b1000d704926afd6a
qianfeng0 <qianfeng0@W-M.local>
2016-04-01 14:50:21 +0800

    第一次创建的项目

---
+++ text1/text1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text1
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 MengWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text1/text1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text1
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 MengWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
